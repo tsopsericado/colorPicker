9c4425f1cf93cae6ab8b8fc0c60dc5f28cfd3886
tsopsericado <ricardotsopse@gmail.com>
2023-04-16 22:13:39 +0100

    some modification made in css

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-// //Get value
-// document.querySelectorAll('input[type=color]').forEach(function(picker){
-//     //Target Point
-//     var targetLabel = document.querySelector('label[for="' + picker.id + '"]');
-//     colorArea = document.createElement(span)
-// });
+//Get value
+document.querySelectorAll('input[type=color]').forEach(function(picker){
+    //Target Point
+    var targetLabel = document.querySelector('label[for="' + picker.id + '"]');
+    colorArea = document.createElement(span);
+
+    colorArea.innerHTML = picker.Value;
+    targetLabel.appendChild(colorArea);
+
+    //Now AddEventListener
+    picker.addEventListener('change', function(){
+        colorArea.innerHTML = picker.value;
+        targetLabel.appendChild(colorArea)
+    });
+    
+});
